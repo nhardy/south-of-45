12efd052104b4a87979d475b0ec5c81d836a23fd
Nathan Hardy <nhardy@live.com.au>
2017-03-18 15:21:31 +1100

    Fix lint error

--- src/app/components/GitHub/Repo/index.js
+++ src/app/components/GitHub/Repo/index.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import cx from 'classnames';
 
 import FontAwesome from 'app/components/FontAwesome';
@@ -7,39 +7,37 @@ import Icon from 'app/components/Icon';
 import styles from './styles.styl';
 
 
-export default class GitHubRepo extends Component {
-  static propTypes = {
-    repo: PropTypes.shape({}),
-  };
+const GitHubRepo = ({ repo }) => {
+  const {
+    name,
+    html_url: url,
+    description,
+    language,
+    stargazers_count: stargazersCount,
+  } = repo;
 
-  render() {
-    const {
-      repo: {
-        name,
-        html_url: url,
-        description,
-        language,
-        stargazers_count: stargazersCount,
-      },
-    } = this.props;
-
-    return (
-      <div className={styles.root}>
-        <h3 className={styles.name}><a href={url} target="_blank" rel="noopener noreferrer">{name}</a></h3>
-        <div className={styles.info}>
-          <p className={styles.description}>{description || 'This repository has no description yet'}</p>
-        </div>
-        <div className={styles.meta}>
-          <div className={styles.language}>
-            <Icon className={styles.icon} name={language.toLowerCase()} />
-            <span>{language}</span>
-          </div>
-          <a className={styles.stargazers} href={`${url}/stargazers`} target="_blank" rel="noopener noreferrer">
-            <FontAwesome className={cx(styles.star, 'fa-star')} />
-            <span>{stargazersCount}</span>
-          </a>
-        </div>
+  return (
+    <div className={styles.root}>
+      <h3 className={styles.name}><a href={url} target="_blank" rel="noopener noreferrer">{name}</a></h3>
+      <div className={styles.info}>
+        <p className={styles.description}>{description || 'This repository has no description yet'}</p>
       </div>
-    );
-  }
-}
+      <div className={styles.meta}>
+        <div className={styles.language}>
+          <Icon className={styles.icon} name={language.toLowerCase()} />
+          <span>{language}</span>
+        </div>
+        <a className={styles.stargazers} href={`${url}/stargazers`} target="_blank" rel="noopener noreferrer">
+          <FontAwesome className={cx(styles.star, 'fa-star')} />
+          <span>{stargazersCount}</span>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+GitHubRepo.propTypes = {
+  repo: PropTypes.shape({}),
+};
+
+export default GitHubRepo;
